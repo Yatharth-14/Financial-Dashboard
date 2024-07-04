d12ec722ef3e5278d962ba63e4ade5a9c7bfc467
Yatharth-14 <yatharthyadav123@gmail.com>
2024-07-04 10:34:50 +0530

    expanded mui theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,21 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles"
+import { useMemo } from "react"
+import { themeSettings } from "./theme"
+import { CssBaseline } from "@mui/material";
+
 function App() {
+  const theme = useMemo(()=> createTheme(themeSettings), []);
   return (
     <>
-      
+      <div className="app">
+        <ThemeProvider theme={theme}>
+          <CssBaseline></CssBaseline>
+          hi
+        </ThemeProvider>
+      </div>
     </>
   )
 }
 
 export default App
+ 
