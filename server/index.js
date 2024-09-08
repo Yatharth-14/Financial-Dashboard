0f1dba3255e68f05825566b1a327b653c5ad5e57
Yatharth-14 <yatharthyadav123@gmail.com>
2024-09-08 22:14:59 +0530

    completed mongo db installation and setup section

--- server/index.js
+++ server/index.js
@@ -7,14 +7,24 @@ import helmet from "helmet";
 import morgan from "morgan";
 
 /* CONFIGURATIONS */
-dotenv.config()
-const app = express()
-app.use(express.json())
-app.use(helmet())
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}))
-app.use(morgan("common"))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 /** MONGOOSE SETUP */
+const PORT = process.env.PORT || 9000;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(async () => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
