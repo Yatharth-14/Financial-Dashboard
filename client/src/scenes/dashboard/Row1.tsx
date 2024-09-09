a73d1815cd6eb2e0b7aa62cf5802cf851d91ff3a
tanujarawat <tanujarawat461@gmail.com>
2024-09-09 14:24:55 +0530

    2:36

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -1,13 +1,78 @@
 import DashboardBox from "@/components/DashboardBox";
+import { useTheme } from '@mui/material/styles';
+import { useMemo } from 'react';
 import { useGetKpisQuery } from "@/states/api";
+import {
+  ResponsiveContainer,
+  AreaChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Area
+}
+  from "recharts";
 
-const Row1 = () => {
+type Props = {};
+
+const Row1 = (props: Props) => {
+  const { palette } = useTheme();
   const { data } = useGetKpisQuery();
   console.log('data:', data);
+  const revenueExpenses = useMemo(() => {
+    return (
+      data &&
+      data[0].monthlyData.map(({ month, revenue, expenses }) => {
+        return {
+          name: month.substring(0, 3),
+          revenue: revenue,
+          expenses: expenses,
+        }
+      })
+    );
+  }, [data])
 
   return (
     <>
-      <DashboardBox gridArea="a"></DashboardBox>
+      <DashboardBox gridArea="a">
+        <ResponsiveContainer width="100%" height={400}>
+          <AreaChart
+            data={revenueExpenses}
+            margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+          >
+            <defs>
+              <linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor={palette.primary.main} stopOpacity={0.8} />
+                <stop offset="95%" stopColor={palette.primary.main} stopOpacity={0} />
+              </linearGradient>
+              <linearGradient id="colorExpenses" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor={palette.secondary.main} stopOpacity={0.8} />
+                <stop offset="95%" stopColor={palette.secondary.main} stopOpacity={0} />
+              </linearGradient>
+            </defs>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis domain={[0, 'dataMax + 5000']} />
+            <Tooltip />
+            <Area
+              type="monotone"
+              dataKey="revenue"
+              stroke={palette.primary.main}
+              fillOpacity={1}
+              fill="url(#colorRevenue)"
+            />
+            <Area
+              type="monotone"
+              dataKey="expenses"
+              stroke={palette.secondary.main}
+              fillOpacity={1}
+              fill="url(#colorExpenses)"
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      </DashboardBox>
+
+
       <DashboardBox gridArea="b"></DashboardBox>
       <DashboardBox gridArea="c"></DashboardBox>
     </>
--- server/routes/kpi.js
+++ server/routes/kpi.js
@@ -3,7 +3,7 @@ import KPI from '../models/KPI.js';
 
 const router = express.Router();
 
-router.get('/kpi', async (req, res) => {
+router.get('/kpis', async (req, res) => {
     try{
         const kpis = await KPI.find();
         res.status(200).json(kpis);
